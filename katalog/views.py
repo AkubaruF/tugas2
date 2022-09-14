4814da8cdea93736a8d8fd98f575a97f46f79aa7
akbar.firdaus <akbar.firdaus@ui.ac.id>
2022-09-14 18:41:52 +0700

    Poin 1-3 Katalog

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # TODO: Create your views here.
+from katalog.models import CatalogItem
+
+def index(request):
+    barang_katalog = CatalogItem.objects.all()
+    response = {'barang_katalog' : barang_katalog}
+    return render(request, 'katalog.html', response)
